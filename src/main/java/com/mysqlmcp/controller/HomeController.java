a8b92e38eef8e829f24b99e64792917f160056fa
Abhay <rabhay780@gmail.com>
2026-07-18 23:30:14 +0530

    Release v0.1.0

--- src/main/java/com/mysqlmcp/controller/HomeController.java
+++ src/main/java/com/mysqlmcp/controller/HomeController.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
@@ -27,5 +29,16 @@ public class HomeController {
         
         log.debug("Unauthenticated user accessing root - redirecting to login");
         return "redirect:/login";
+    }
+
+    /**
+     * MCP clients or external tools may send a POST to the root URL as a health
+     * check or initialization request. Return a 200 with a simple status instead
+     * of crashing with a 405 Method Not Allowed.
+     */
+    @PostMapping("/")
+    @ResponseBody
+    public ResponseEntity<String> rootPost() {
+        return ResponseEntity.ok("OK");
     }
 }
